a716ae638650b51878f7fab264d54ee7d96b8f59
Thomas insaurralde <tomyalejoroldan@gmail.com>
2022-09-29 22:53:26 -0300

    proyecto

---
+++ main.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".botton1")
+btn.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn2 = document.querySelector(".botton2")
+btn2.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn3 ={ ...btn3}
