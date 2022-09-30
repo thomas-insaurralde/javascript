24274a9af66d81b55aadeadd583cbd281035eb55
Thomas insaurralde <tomyalejoroldan@gmail.com>
2022-09-29 23:24:08 -0300

    proyecto

--- main.js
+++ main.js
@@ -16,4 +16,119 @@ btn2.addEventListener('click', () => {
   )
 })
 
-const btn3 ={ ...btn3}
+const btn3 = document.querySelector(".botton3")
+btn3.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn4 = document.querySelector(".botton4")
+btn4.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn5 = document.querySelector(".botton5")
+btn5.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn6 = document.querySelector(".botton6")
+btn6.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+
+const btn7 = document.querySelector(".botton7")
+btn7.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn8 = document.querySelector(".botton8")
+btn8.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn9 = document.querySelector(".botton9")
+btn9.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn10 = document.querySelector(".botton10")
+btn10.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn11 = document.querySelector(".botton11")
+btn11.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn12 = document.querySelector(".botton12")
+btn12.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn13 = document.querySelector(".botton12")
+btn12.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn14 = document.querySelector(".botton12")
+btn12.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn15 = document.querySelector(".botton12")
+btn12.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
+const btn16 = document.querySelector(".botton12")
+btn12.addEventListener('click', () => {
+  Swal.fire(
+    'Genial!',
+    'Agregaste el producto al carrito',
+    'success'
+  )
+})
